00a696fa5282cf0d04d157d502cc59e1b26ae1c8
Almir Banjanovic <abanjanovic@microsoft.com>
2025-08-29 12:13:49 -0400

    Shut down ai-foundry

--- environments/ai-foundry/terraform/main.tf
+++ environments/ai-foundry/terraform/main.tf
@@ -1,15 +1,15 @@
-resource "azurerm_ai_services" "this" {
-  resource_group_name                = var.resource_group_name
-  custom_subdomain_name              = "ais-cloud-playground-eastus2"
-  location                           = "eastus2"
-  name                               = "ais-cloud-playground-eastus2"
-  sku_name                           = "S0"
+# resource "azurerm_ai_services" "this" {
+#   resource_group_name                = var.resource_group_name
+#   custom_subdomain_name              = "ais-cloud-playground-eastus2"
+#   location                           = "eastus2"
+#   name                               = "ais-cloud-playground-eastus2"
+#   sku_name                           = "S0"
   
-  identity {
-    type         = "SystemAssigned"
-  }
+#   identity {
+#     type         = "SystemAssigned"
+#   }
   
-  network_acls {
-    default_action = "Allow"
-  }
-}
+#   network_acls {
+#     default_action = "Allow"
+#   }
+# }
