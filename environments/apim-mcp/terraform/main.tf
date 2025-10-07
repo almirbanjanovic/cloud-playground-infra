30e493c3f8d0a0d633769cdaa864e5f27657e0a4
Almir Banjanovic <abanjanovic@microsoft.com>
2025-10-07 13:16:30 -0400

    Initial apim-mcp commit.

--- environments/apim-mcp/terraform/main.tf
+++ environments/apim-mcp/terraform/main.tf
@@ -9,3 +9,19 @@ module "apim" {
   sku_name            = "BasicV2"
   sku_count           = 1
 }
+
+resource "azurerm_api_management_api" "colors-api" {
+  name                = "colors-api"
+  resource_group_name = var.resource_group_name
+  api_management_name = module.apim.api_management_service_name
+  revision            = "1"
+  display_name        = "Colors API"
+  protocols           = ["https"]
+  service_url = "https://colors-api.azurewebsites.net/"
+  path = "colors"
+
+  import {
+    content_format = "openapi+json-link"
+    content_value  = "https://colors-api.azurewebsites.net/swagger/v1/swagger.json"
+  }
+}
