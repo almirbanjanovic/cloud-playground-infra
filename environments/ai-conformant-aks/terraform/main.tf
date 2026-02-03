987df9f18027f5efe56f02a1b7fec21ea271b806
Almir Banjanovic <abanjanovic@microsoft.com>
2026-02-03 17:18:43 -0500

    Add crd for custom cpu model for KAITO.

--- environments/ai-conformant-aks/terraform/main.tf
+++ environments/ai-conformant-aks/terraform/main.tf
@@ -541,7 +541,6 @@ resource "kubernetes_manifest" "custom_model" {
       {
         name         = "smollm2-workspace"
         namespace    = kubernetes_namespace_v1.custom_cpu_model.metadata[0].name
-        instanceType = local.gpu_nodepool_vm_size
         agentpool    = local.gpu_nodepool
       }
     )
